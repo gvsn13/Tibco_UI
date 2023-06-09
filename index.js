912077b0620e9116a8075e2fa80dfdf139e6ad4a
gvsn13 <86527158+gvsn13@users.noreply.github.com>
2023-06-09 14:58:17 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+const inputBox = document.querySelector(".inputi");
+const display = document.querySelector(".displayp");
+const inputbt = document.querySelector(".btnok");
+
+async function getqueue(queueName){
+    alert(queueName);
+    const url = `${queueName}`;
+    // const headers = {
+    //     Authorization: "Bearer CIC~OXMxtvy3D60qvyINr0clRqIL"
+    // }
+
+    const response = await fetch(url, {
+        method: 'GET',
+        headers: {
+        Authorization: "Bearer CIC~OXMxtvy3D60qvyINr0clRqIL"
+        }
+    })
+    .then(resp => resp.json())
+    .then(json=>console.log(JSON.stringify(json)));
+
+}
+
+inputbt.addEventListener("click", ()=>{
+    getqueue(inputBox.value);
+})
